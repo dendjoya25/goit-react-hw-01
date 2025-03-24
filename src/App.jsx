1bb634eb0ac807e7fbc798da9d728b2fe832c74d
Den_Djoya <djoya.den@gmail.com>
2025-03-24 15:31:51 +0100

    change App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
 import Profile from "./components/Profile/Profile";
-import { FriendList } from "./components/FriendList/FriendList.jsx";
-import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import s from "./App.module.css";
 
 function App() {
